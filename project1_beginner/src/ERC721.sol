e036138b1b7d928d4e14736dcd3fc8c4128cb9bc
mlu@1 <mthandemeluleki@yahoo.com>
2024-04-24 13:06:00 +0800

    Prepare to deploy on local network

--- project1_beginner/src/ERC721.sol
+++ project1_beginner/src/ERC721.sol
@@ -1,3 +1,18 @@
+pragma solidity ^0.8.13;
 contract DevconPanda is ERC721URIStorage{
-    using Counters for Counters.Counter
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+
+    constructor () ERC721{"DevconPanda","DCP"}{}
+
+    function mint(address user,string memory tokenURI) public return (uint256){
+        uint256 newItemId = _tokenIds.current();
+        _mint(user ,newItemId);
+        _setTokenURL(newItemId,tokenURI);
+
+        _tokenIds.increment();
+        return newItemIds 
+        
+    }
 }
